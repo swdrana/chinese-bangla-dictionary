68e7570e540485115c2d71d9f2bfd97e174005ab
swdrana <tctr2s@gmail.com>
2022-09-07 00:32:44 +0600

    installed tailwind component and add-word page added with form

--- src/App.js
+++ src/App.js
@@ -1,23 +1,17 @@
-import logo from './logo.svg';
+import { Route, Routes } from 'react-router-dom';
 import './App.css';
+import AddWord from './components/pages/AddWord';
+import Home from './components/pages/Home';
+import Nav from './components/shared/Nav';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Nav/>
+      <Routes>
+        <Route path='/' element={<Home/>}/>
+        <Route path='/add-word' element={<AddWord/>}/>
+      </Routes>
     </div>
   );
 }
---
+++ src/components/pages/AddWord.js
@@ -0,0 +1,78 @@
+import { Button, Label, TextInput } from "flowbite-react";
+import React, { useState } from "react";
+
+const AddWord = () => {
+  const [hanzi, setHanzi] = useState('')
+  const [pinyin, setPinyin] = useState('')
+  const [english, setEnglish] = useState('')
+  const [bangla, setBangla] = useState('')
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    const word = {hanzi, pinyin, english, bangla}
+    console.log(word);
+  };
+  return (
+    <div className=" w-1/2 mx-auto">
+      <h1 className=" text-2xl text-center">Add Word</h1>
+
+      <form className="flex flex-col gap-4" onSubmit={handleSubmit}>
+        <div>
+          <div className="mb-2 block">
+            <Label htmlFor="hanzi" value="Input Hanzi" />
+          </div>
+          <TextInput
+            id="hanzi"
+            type="text"
+            placeholder="Input Chinese Character"
+            required={false} 
+            onChange={(e)=>setHanzi(e.target.value)} 
+          />
+        </div>
+
+        <div>
+          <div className="mb-2 block">
+            <Label htmlFor="pinyin" value="Input Pinyin" />
+          </div>
+          <TextInput
+            id="pinyin"
+            type="text"
+            placeholder="Input Chinese Pinyin"
+            required={false}
+            onChange={(e)=>setPinyin(e.target.value)} 
+          />
+        </div>
+        <div>
+          <div className="mb-2 block">
+            <Label htmlFor="english" value="Input English Meaning" />
+          </div>
+          <TextInput
+            id="english"
+            type="text"
+            placeholder="Input English Meaning"
+            required={false}
+            onChange={(e)=>setEnglish(e.target.value)} 
+          />
+        </div>
+        <div>
+          <div className="mb-2 block">
+            <Label htmlFor="bangla" value="Bangla Meaning" />
+          </div>
+          <TextInput
+            id="bangla"
+            type="text"
+            placeholder="Input Bangla Meaning"
+            required={false}
+            onChange={(e)=>setBangla(e.target.value)} 
+          />
+        </div>
+        {/* <div className="flex items-center gap-2">
+          <Checkbox id="remember" />
+          <Label htmlFor="remember">Remember me</Label>
+        </div> */}
+        <Button type="submit">Submit</Button>
+      </form>
+    </div>
+  );
+};
+
+export default AddWord;
